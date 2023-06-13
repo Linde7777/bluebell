3ece990aeff89b80bfa91e5c5965456576dbe955
LongPeng <longpeng96@qq.com>
2023-06-13 17:15:22 +0800

    update

---
+++ pkg/snowflake/snowflake.go
@@ -0,0 +1,24 @@
+package snowflake
+
+import (
+	"time"
+
+	sf "github.com/bwmarrin/snowflake"
+)
+
+var node *sf.Node
+
+func Init(startTime string, machineID int64) (err error) {
+	var st time.Time
+	st, err = time.Parse("2023-01-01", startTime)
+	if err != nil {
+		return
+	}
+	sf.Epoch = st.UnixNano() / 1000000
+	node, err = sf.NewNode(machineID)
+	return err
+}
+
+func GenID() int64 {
+	return node.Generate().Int64()
+}
