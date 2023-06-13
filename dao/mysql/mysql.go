9ba6c4eb93f03c944987e69a3b90d9cd6e044fc3
LongPeng <longpeng96@qq.com>
2023-06-13 16:13:21 +0800

    update

---
+++ dao/mysql/mysql.go
@@ -0,0 +1,39 @@
+package mysql
+
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+
+	"github.com/spf13/viper"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+var db *sqlx.DB
+
+func Init() (err error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+		viper.GetString("mysql.user"),
+		viper.GetString("mysql.password"),
+		viper.GetString("mysql.host"),
+		viper.GetInt("mysql.port"),
+		viper.GetString("mysql.dbname"),
+	)
+
+	if db, err = sqlx.Connect("mysql", dsn); err != nil {
+		zap.L().Error("sqlx.Connect failed: ", zap.Error(err))
+		return
+	}
+
+	db.SetMaxOpenConns(viper.GetInt("mysql.max_conn"))
+	db.SetMaxIdleConns(viper.GetInt("mysql.idle_conn"))
+	return
+}
+
+func Close() {
+	if err := db.Close(); err != nil {
+		zap.L().Fatal("fail to close mysql", zap.Error(err))
+	}
+}
