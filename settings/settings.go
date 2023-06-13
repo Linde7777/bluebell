865403211ce46f7e22905aa168a9f93bb7a635d9
LongPeng <longpeng96@qq.com>
2023-06-13 20:07:08 +0800

    add structure mapping

--- settings/settings.go
+++ settings/settings.go
@@ -8,6 +8,44 @@ import (
 	"github.com/spf13/viper"
 )
 
+var Conf = new(AppConfig)
+
+type AppConfig struct {
+	Name         string `mapstructure:"name"`
+	Mode         string `mapstructure:"mode"`
+	Version      string `mapstructure:"version"`
+	Port         int    `mapstructure:"port"`
+	*LogConfig   `mapstructure:"log"`
+	*MySQLConfig `mapstructure:"mysql"`
+	*RedisConfig `mapstructure:"redis"`
+}
+
+type LogConfig struct {
+	Level      string `mapstructure:"level"`
+	Filename   string `mapstructure:"filename"`
+	MaxSize    int    `mapstructure:"max_size"`
+	MaxAge     int    `mapstructure:"max_age"`
+	MaxBackups int    `mapstructure:"max_backups"`
+}
+
+type MySQLConfig struct {
+	Host         string `mapstructure:"host"`
+	User         string `mapstructure:"user"`
+	Password     string `mapstructure:"password"`
+	DBName       string `mapstructure:"dbname"`
+	Port         int    `mapstructure:"port"`
+	MaxOpenConns int    `mapstructure:"max_conns"`
+	MaxIdleConns int    `mapstructure:"idle_conns"`
+}
+
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	Password string `mapstructure:"password"`
+	Port     int    `mapstructure:"port"`
+	DB       int    `mapstructure:"db"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
+
 func Init() (err error) {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
@@ -16,10 +54,17 @@ func Init() (err error) {
 		fmt.Println("viper.ReadInConfig() failed: ", err)
 		return
 	}
+
+	if err = viper.Unmarshal(Conf); err != nil {
+		fmt.Println("viper.Unmarshal failed: ", err)
+	}
+
 	viper.WatchConfig()
 	viper.OnConfigChange(func(in fsnotify.Event) {
 		fmt.Println("Configuration file has been modified...")
-
+		if err := viper.Unmarshal(Conf); err != nil {
+			fmt.Println("viper.Unmarshal failed: ", err)
+		}
 	})
 	return
 }
