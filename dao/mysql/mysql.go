54c8f3dcd08b2f5d90fe5928ddec55012a7a46be
LongPeng <longpeng96@qq.com>
2023-06-14 16:23:30 +0800

    rename variable

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -14,13 +14,13 @@ import (
 
 var db *sqlx.DB
 
-func Init(cf *settings.MySQLConfig) (err error) {
+func Init(cfg *settings.MySQLConfig) (err error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
-		cf.User,
-		cf.Password,
-		cf.Host,
-		cf.Port,
-		cf.DBName,
+		cfg.User,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.DBName,
 	)
 
 	if db, err = sqlx.Connect("mysql", dsn); err != nil {
