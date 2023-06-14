dd9b5f47166caf38d0045cad140557bb3be5d7f5
LongPeng <longpeng96@qq.com>
2023-06-14 14:00:35 +0800

    SignUpHandler: get params

--- controller/user.go
+++ controller/user.go
@@ -2,16 +2,28 @@ package controller
 
 import (
 	"bluebell/logic"
+	"bluebell/models"
 	"net/http"
+
+	"go.uber.org/zap"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SignUpHandler(c *gin.Context) {
-	// 1. get argument and validate
+	// 1. get parameters and validate them
+	var p models.ParamSignUp
+	if err := c.ShouldBindJSON(&p); err != nil {
+		zap.L().Error("SignUp with invalid param", zap.Error(err))
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "invalid parameters",
+		})
+		return
+	}
+
 	// 2. logic
 	logic.SignUp()
 
 	// 3. return
-	c.JSON(http.StatusOK, gin.H{"msg": "you have sign up"})
+	c.JSON(http.StatusOK, gin.H{"msg": "sign up success"})
 }
