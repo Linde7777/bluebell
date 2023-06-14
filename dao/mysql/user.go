5eb7bdee6127d70ad67b36f3e3927cc796d7ab9c
LongPeng <longpeng96@qq.com>
2023-06-14 15:59:15 +0800

    InsertUser finished

--- dao/mysql/user.go
+++ dao/mysql/user.go
@@ -1,6 +1,27 @@
 package mysql
 
+import (
+	"bluebell/models"
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+)
+
 func QueryUserByID() {
+
+}
+
+func CheckUserExist(username string) (err error) {
+	sqlStr := "select count(user_id) from user where username=?"
+	var count int
+	if err = db.Get(&count, sqlStr, username); err != nil {
+		return err
+	}
+	if count > 0 {
+		return errors.New("user is already exists")
+	}
+
+	return
 
 }
 
@@ -8,6 +29,19 @@ func QueryUserByUsername() {
 
 }
 
-func SignUp() {
+// InsertUser will encrypt the models.User.Password
+func InsertUser(u *models.User) error {
+	u.Password = encrypt(u.Password)
 	// 1. run sql
+	sqlStr := "insert into user(user_id,username,password) values(?,?,?)"
+	_, err := db.Exec(sqlStr, u.UserID, u.Username, u.Password)
+	return err
+}
+
+const secret = "longpeng96@gmail.com"
+
+func encrypt(password string) string {
+	h := md5.New()
+	h.Write([]byte(secret))
+	return hex.EncodeToString(h.Sum([]byte(password)))
 }
