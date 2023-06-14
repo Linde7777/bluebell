a2ccc7e11c93442bbaaf4a2e6986e80912b5a61d
LongPeng <longpeng96@qq.com>
2023-06-14 19:24:44 +0800

    fix bug

--- models/userlogin.go
+++ models/userlogin.go
@@ -2,6 +2,6 @@ package models
 
 // UserLogin store the info that is needed to be login
 type UserLogin struct {
-	Username string `db:"username"`
-	Password string `db:"password"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
