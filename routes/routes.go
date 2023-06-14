962ed45eda38f964e7e27836788c4b06117ca6e6
LongPeng <longpeng96@qq.com>
2023-06-14 16:47:40 +0800

    add mode in SetUp

--- routes/routes.go
+++ routes/routes.go
@@ -8,7 +8,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetUp() (engine *gin.Engine) {
+func SetUp(mode string) (engine *gin.Engine) {
+	if mode == gin.ReleaseMode {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	r := gin.New()
 	r.Use(logger.GinLogger(), logger.GinRecovery(true))
 	r.GET("/", func(c *gin.Context) {
