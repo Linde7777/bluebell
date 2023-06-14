6697141e6d02bbd8bd9504b823595157a80d8a09
LongPeng <longpeng96@qq.com>
2023-06-14 14:53:56 +0800

    ParamSignUp: add binding and eqfield

--- models/params.go
+++ models/params.go
@@ -2,7 +2,7 @@ package models
 
 // ParamSignUp define the parameters structure
 type ParamSignUp struct {
-	Username   string `json:"username"`
-	Password   string `json:"password"`
-	RePassword string `json:"re_password"`
+	Username   string `json:"username" binding:"required"`
+	Password   string `json:"password" binding:"required"`
+	RePassword string `json:"re_password" binding:"required,eqfield=Password"`
 }
