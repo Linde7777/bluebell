957336767c165e1748a06c81d7fbc687b1440e8a
LongPeng <longpeng96@qq.com>
2023-06-14 14:24:53 +0800

    SignUpHandler: validate params

--- controller/user.go
+++ controller/user.go
@@ -12,17 +12,34 @@ import (
 
 func SignUpHandler(c *gin.Context) {
 	// 1. get parameters and validate them
-	var p models.ParamSignUp
-	if err := c.ShouldBindJSON(&p); err != nil {
-		zap.L().Error("SignUp with invalid param", zap.Error(err))
+	p := new(models.ParamSignUp)
+	if err := c.ShouldBindJSON(p); err != nil {
+		msg := "SignUp failed to bind JSON"
+		zap.L().Error(msg, zap.Error(err))
 		c.JSON(http.StatusOK, gin.H{
-			"msg": "invalid parameters",
+			"msg": msg,
 		})
 		return
 	}
 
+	if len(p.Password) == 0 || len(p.RePassword) == 0 || len(p.Username) == 0 {
+		msg := "SignUp with empty parameters"
+		zap.L().Error(msg)
+		c.JSON(http.StatusOK, gin.H{
+			"msg": msg,
+		})
+	}
+
+	if p.Password != p.RePassword {
+		msg := "Password does not match RePassword"
+		zap.L().Error(msg)
+		c.JSON(http.StatusOK, gin.H{
+			"msg": msg,
+		})
+	}
+
 	// 2. logic
-	logic.SignUp()
+	logic.SignUp(p)
 
 	// 3. return
 	c.JSON(http.StatusOK, gin.H{"msg": "sign up success"})
