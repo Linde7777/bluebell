52de4d2b99a9176998de73829372ba3af0dc6665
LongPeng <longpeng96@qq.com>
2023-06-14 13:09:14 +0800

    modify argument

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -14,13 +14,13 @@ import (
 
 var db *sqlx.DB
 
-func Init() (err error) {
+func Init(cf *settings.MySQLConfig) (err error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
-		settings.Conf.MySQLConfig.User,
-		settings.Conf.MySQLConfig.Password,
-		settings.Conf.MySQLConfig.Host,
-		settings.Conf.MySQLConfig.Port,
-		settings.Conf.MySQLConfig.DBName,
+		cf.User,
+		cf.Password,
+		cf.Host,
+		cf.Port,
+		cf.DBName,
 	)
 
 	if db, err = sqlx.Connect("mysql", dsn); err != nil {
