10faeb1302256b042e2ebab4ecd188a6c418d1a5
LongPeng <longpeng96@qq.com>
2023-06-15 16:08:51 +0800

    update, AccessToken and RefreshToken, v1

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -7,7 +7,8 @@ import (
 )
 
 var secret = []byte("Red Read Redemption II")
-var TokenExpireDuration = time.Hour * 2
+var AccTokenExpireDuration = time.Minute * 10
+var RefTokenExpireDuration = time.Hour * 24 * 30
 var ErrInvalidToken = errors.New("invalid token")
 
 type MyClaims struct {
@@ -16,21 +17,39 @@ type MyClaims struct {
 	Username string `json:"username"`
 }
 
-func GenToken(userID int64, username string) (string, error) {
+func GenToken(userID int64, username string) (accessToken, refreshToken string, err error) {
 	mc := &MyClaims{
 		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(TokenExpireDuration).Unix(),
+			ExpiresAt: time.Now().Add(AccTokenExpireDuration).Unix(),
 			Issuer:    "bluebell",
 		},
 		UserID:   userID,
 		Username: username,
 	}
-
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, mc)
-	return token.SignedString(secret)
+	accessToken, err = token.SignedString(secret)
+	if err != nil {
+		return "", "", err
+	}
+
+	token = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(RefTokenExpireDuration).Unix(),
+		Issuer:    "bluebell",
+	})
+	refreshToken, err = token.SignedString(secret)
+	if err != nil {
+		return "", "", err
+	}
+
+	return
 }
 
+// ParseToken can parse both AccessToken and RefreshToken,
+// because RefreshToken is represented in jwt.StandardClaims,
+// and AccessToken, which represent in MyClaims, contain
+// jwt.StandardClaims too
 func ParseToken(tokenStr string) (*MyClaims, error) {
+
 	var mc = new(MyClaims)
 	token, err := jwt.ParseWithClaims(tokenStr, mc,
 		func(token *jwt.Token) (interface{}, error) {
