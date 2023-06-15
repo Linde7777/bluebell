5fcc421e91992c78643a60394242fe14551e8a0d
LongPeng <longpeng96@qq.com>
2023-06-15 15:02:02 +0800

    update

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,9 +1,9 @@
 package middlewares
 
 import (
+	"bluebell/controller"
 	"bluebell/pkg/jwt"
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"strings"
 )
 
@@ -13,20 +13,14 @@ func JWTAuthMiddleware() func(c *gin.Context) {
 		// Authorization: Bearer: xxx.xxx.xxx
 		authHeader := c.Request.Header.Get("Authorization")
 		if authHeader == "" {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2003,
-				"msg":  "auth is empty",
-			})
+			controller.ResponseError(c, controller.CodeNeedLogin)
 			c.Abort()
 			return
 		}
 
 		parts := strings.SplitN(authHeader, " ", 2)
 		if !(len(parts) == 2 && parts[0] == "Bearer") {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2004,
-				"msg":  "wrong auth format",
-			})
+			controller.ResponseError(c, controller.CodeInvalidToken)
 			c.Abort()
 			return
 		}
@@ -34,15 +28,14 @@ func JWTAuthMiddleware() func(c *gin.Context) {
 		// parts[1] is tokenString
 		mc, err := jwt.ParseToken(parts[1])
 		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2005,
-				"msg":  "invalid token",
-			})
+			controller.ResponseError(c, controller.CodeInvalidToken)
 			c.Abort()
 			return
 		}
 
-		c.Set("username", mc.Username)
+		c.Set(CtxUserIDKey, mc.UserID)
 		c.Next()
 	}
 }
+
+const CtxUserIDKey = "userID"
