f591c3285ccb227e01bb69d3b4f29a508bbe2baf
LongPeng <longpeng96@qq.com>
2023-06-16 16:50:57 +0800

    fix typo

--- routes/routes.go
+++ routes/routes.go
@@ -20,6 +20,7 @@ func SetUp(mode string) (engine *gin.Engine) {
 	v1.Use(middlewares.JWTAuthMiddleware())
 	{
 		v1.GET("/community", controller.CommunityHandler)
+		v1.GET("/community/:id", controller.CommunityDetailHandler)
 	}
 	return r
 }
