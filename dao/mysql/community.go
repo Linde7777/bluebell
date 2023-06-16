0aa7a4b3466fc13eaefce083d0db0332b6b39330
LongPeng <longpeng96@qq.com>
2023-06-16 16:52:19 +0800

    rename model

--- dao/mysql/community.go
+++ dao/mysql/community.go
@@ -6,7 +6,7 @@ import (
 	"go.uber.org/zap"
 )
 
-func GetCommunityList() (communityList []*models.CommunitySelected, err error) {
+func GetCommunityList() (communityList []*models.CommunityBasicSelected, err error) {
 	sqlStr := "select community_id,community_name from community"
 	if err = db.Select(&communityList, sqlStr); err != nil {
 		if err == sql.ErrNoRows {
