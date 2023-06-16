f5925023b0661459495f0e717d4cc027c94a3168
LongPeng <longpeng96@qq.com>
2023-06-16 16:52:03 +0800

    GetCommunityDetailList finished

--- logic/community.go
+++ logic/community.go
@@ -8,3 +8,12 @@ import (
 func GetCommunityList() (communityList []*models.CommunityBasicSelected, err error) {
 	return mysql.GetCommunityList()
 }
+
+func GetCommunityDetailList(id int64) (*models.CommunityDetailSelected, error) {
+	data, rowIsEmpty, err := mysql.GetCommunityDetailList(id)
+	if rowIsEmpty && err == nil {
+		return nil, nil
+	}
+	return data, err
+
+}
