f74b338809d9eedd917778f1fbf1036fb68f4e53
LongPeng <longpeng96@qq.com>
2023-06-16 16:14:33 +0800

    add struct CommunityDetailSelected

--- models/community.go
+++ models/community.go
@@ -6,3 +6,11 @@ type CommunitySelected struct {
 	ID   int    `json:"id" db:"community_id"`
 	Name string `json:"name" db:"community_name"`
 }
+
+// CommunityDetailSelected store the community
+// detail info, where come from SQL query
+type CommunityDetailSelected struct {
+	ID           int    `json:"id" db:"community_id"`
+	Name         string `json:"name" db:"community_name"`
+	Introduction string `json:"introduction" db:"introduction"`
+}
