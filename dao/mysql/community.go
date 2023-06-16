306c477e9d4ce987105fd558d9f82357acf8378d
LongPeng <longpeng96@qq.com>
2023-06-16 16:52:58 +0800

    GetCommunityDetail

--- dao/mysql/community.go
+++ dao/mysql/community.go
@@ -17,3 +17,23 @@ func GetCommunityList() (communityList []*models.CommunityBasicSelected, err err
 
 	return
 }
+
+// GetCommunityDetail will return rowIsEmpty:true
+// and error:nil if it meets sql.ErrNoRows
+func GetCommunityDetail(id int64) (
+	communityDetail *models.CommunityDetailSelected,
+	rowIsEmpty bool, err error) {
+
+	communityDetail = new(models.CommunityDetailSelected)
+	sqlStr := "select community_id,community_name,introduction," +
+		"create_time from community where community_id=?"
+	if err = db.Get(communityDetail, sqlStr, id); err != nil {
+		if err == sql.ErrNoRows {
+			err = nil
+			return nil, true, err
+		}
+		return nil, false, err
+	}
+
+	return
+}
