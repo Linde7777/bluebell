e751023bbd1ab37c1c78cc593377c2c6118f8943
LongPeng <longpeng96@qq.com>
2023-06-16 16:16:33 +0800

    rename model

--- models/community.go
+++ models/community.go
@@ -1,8 +1,8 @@
 package models
 
-// CommunitySelected store the community
+// CommunityBasicSelected store the community
 // basic info, where come from SQL query
-type CommunitySelected struct {
+type CommunityBasicSelected struct {
 	ID   int    `json:"id" db:"community_id"`
 	Name string `json:"name" db:"community_name"`
 }
