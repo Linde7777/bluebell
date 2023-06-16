5da78d12bff1ab14d1a9bfb4ffde27518a6776ec
LongPeng <longpeng96@qq.com>
2023-06-16 13:13:38 +0800

    created

---
+++ models/create_community_table.sql
@@ -0,0 +1,14 @@
+create table community
+(
+    id             int(11)                                 not null auto_increment,
+    community_id   int(10) unsigned                        not null,
+    community_name varchar(128) collate utf8mb4_general_ci not null,
+    introduction   varchar(256) collate utf8mb4_general_ci not null,
+    create_time    timestamp                               not null default current_timestamp,
+    update_time    timestamp                               not null default current_timestamp on update current_timestamp,
+    primary key (id),
+    unique key idx_community_id (community_id),
+    unique key idx_community_name (community_name)
+) engine = InnoDB
+  default charset = utf8mb4
+  collate = utf8mb4_general_ci
