44921f3f4d68b6ddb0a7ac5543d63d3e67d9e81a
LongPeng <longpeng96@qq.com>
2023-06-16 16:14:08 +0800

    update comment

--- models/community.go
+++ models/community.go
@@ -1,7 +1,7 @@
 package models
 
-// CommunitySelected store the community info,
-// where come from SQL query
+// CommunitySelected store the community
+// basic info, where come from SQL query
 type CommunitySelected struct {
 	ID   int    `json:"id" db:"community_id"`
 	Name string `json:"name" db:"community_name"`
