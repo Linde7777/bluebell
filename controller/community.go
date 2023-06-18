d6e6a686cd0cedb3c9894afa9c18a69acd240845
LongPeng <longpeng96@qq.com>
2023-06-18 16:47:08 +0800

    add log

--- controller/community.go
+++ controller/community.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"bluebell/logic"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"strconv"
 )
 
@@ -11,6 +12,7 @@ import (
 func CommunityHandler(c *gin.Context) {
 	communityList, err := logic.GetCommunityList()
 	if err != nil {
+		zap.L().Error("logic.GetCommunityList: ", zap.Error(err))
 		ResponseError(c, CodeServerBusy)
 		return
 	}
@@ -21,12 +23,14 @@ func CommunityDetailHandler(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.ParseInt(idStr, 10, 64)
 	if err != nil {
+		zap.L().Error("CommunityDetailHandler: ", zap.Error(err))
 		ResponseError(c, CodeInvalidParam)
 		return
 	}
 	communityDetail, err := logic.GetCommunityDetail(id)
 	if err != nil {
-		ResponseError(c, CodeInvalidParam)
+		zap.L().Error("logic.GetCommunityDetail: ", zap.Error(err))
+		ResponseError(c, CodeServerBusy)
 		return
 	}
 	ResponseSuccess(c, communityDetail)
