2ed99c787a0cff402c440f4499d1f391549a1fcc
LongPeng <longpeng96@qq.com>
2023-06-18 19:16:07 +0800

    GetPostDetailByID update, provide more info of a post

--- logic/post.go
+++ logic/post.go
@@ -11,6 +11,26 @@ func CreatePost(pc *models.Post) error {
 	return mysql.InsertPost(pc)
 }
 
-func GetPostDetailByID(id int64) (*models.Post, error) {
-	return mysql.GetPostDetailByID(id)
+func GetPostDetailByID(id int64) (*models.ApiPostDetail, error) {
+	postData, err := mysql.GetPostDetailByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	userData, err := mysql.GetUserByID(postData.AuthorID)
+	if err != nil {
+		return nil, err
+	}
+
+	communityData, rowIsEmpty, err := mysql.GetCommunityDetail(postData.CommunityID)
+	if rowIsEmpty && err == nil {
+		return nil, err
+	}
+
+	apd := new(models.ApiPostDetail)
+	apd.CommunityDetailSelected = communityData
+	apd.Post = postData
+	apd.AuthorName = userData.Username
+
+	return apd, err
 }
