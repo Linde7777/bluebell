950620c192ce95fa871faa2bdacd5c7cceff9bb5
LongPeng <longpeng96@qq.com>
2023-06-18 15:18:36 +0800

    add binding

--- models/post.go
+++ models/post.go
@@ -6,10 +6,10 @@ import "time"
 // to create a post
 type PostCreated struct {
 	ID          int64     `json:"id" db:"post_id"`
-	AuthorID    int64     `json:"author_id" db:"author_id"`
-	CommunityID int64     `json:"community_id" db:"community_id"`
+	AuthorID    int64     `json:"author_id" db:"author_id" binding:"required"`
+	CommunityID int64     `json:"community_id" db:"community_id binding:"required"`
 	Status      int32     `json:"status" db:"status"`
-	Title       string    `json:"title" db:"title"`
-	Content     string    `json:"content" db:"content"`
+	Title       string    `json:"title" db:"title" binding:"required"`
+	Content     string    `json:"content" db:"content" binding:"required"`
 	CreateTime  time.Time `json:"create_time" db:"create_time"`
 }
