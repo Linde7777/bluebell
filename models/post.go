bf7f9fd55e43d09bc12320a3701c84ba79e544b5
LongPeng <longpeng96@qq.com>
2023-06-18 16:56:12 +0800

    fix bug

--- models/post.go
+++ models/post.go
@@ -7,7 +7,7 @@ import "time"
 type Post struct {
 	ID          int64     `json:"id" db:"post_id"`
 	AuthorID    int64     `json:"author_id" db:"author_id"`
-	CommunityID int64     `json:"community_id" db:"community_id binding:"required"`
+	CommunityID int64     `json:"community_id" db:"community_id" binding:"required"`
 	Status      int32     `json:"status" db:"status"`
 	Title       string    `json:"title" db:"title" binding:"required"`
 	Content     string    `json:"content" db:"content" binding:"required"`
