e51b162d4e5da2044ecb57c05af57a4e7e0401fe
LongPeng <longpeng96@qq.com>
2023-06-18 16:41:26 +0800

    GetPostDetailByID finish

--- dao/mysql/post.go
+++ dao/mysql/post.go
@@ -9,3 +9,11 @@ func InsertPost(pc *models.Post) error {
 		pc.AuthorID, pc.CommunityID)
 	return err
 }
+
+func GetPostDetailByID(id int64) (*models.Post, error) {
+	sqlStr := "select post_id,title,content,author_id," +
+		"community_id,create_time from post where post_id=?"
+	p := new(models.Post)
+	err := db.Get(p, sqlStr, id)
+	return p, err
+}
