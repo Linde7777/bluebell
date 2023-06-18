df0ec780bef2f891be27033c391f23160017aee6
LongPeng <longpeng96@qq.com>
2023-06-18 16:40:03 +0800

    add route GetPostDetail

--- routes/routes.go
+++ routes/routes.go
@@ -22,6 +22,7 @@ func SetUp(mode string) (engine *gin.Engine) {
 		v1.GET("/community", controller.CommunityHandler)
 		v1.GET("/community/:id", controller.CommunityDetailHandler)
 		v1.POST("/post", controller.CreatePostHandler)
+		v1.GET("/post/:id", controller.GetPostDetailHandler)
 	}
 	return r
 }
