b06a73e6d2093570ebfc3267afb94bc2ebd00e03
LongPeng <longpeng96@qq.com>
2023-06-18 16:40:17 +0800

    GetPostDetailHandler finish

--- controller/post.go
+++ controller/post.go
@@ -4,11 +4,12 @@ import (
 	"bluebell/logic"
 	"bluebell/models"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func CreatePostHandler(c *gin.Context) {
 	// 1. get parameter
-	pc := new(models.PostCreated)
+	pc := new(models.Post)
 	if err := c.ShouldBindJSON(pc); err != nil {
 		ResponseErrorWithMsg(c, CodeInvalidParam, err.Error())
 	}
@@ -26,4 +27,22 @@ func CreatePostHandler(c *gin.Context) {
 	}
 
 	ResponseSuccess(c, nil)
+}
+
+func GetPostDetailHandler(c *gin.Context) {
+	// get params
+	idStr := c.Param("id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		ResponseErrorWithMsg(c, CodeInvalidParam, err.Error())
+		return
+	}
+
+	data, err := logic.GetPostDetailByID(id)
+	if err != nil {
+		ResponseErrorWithMsg(c, CodeServerBusy, err.Error())
+		return
+	}
+
+	ResponseSuccess(c, data)
 }
