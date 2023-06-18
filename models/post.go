6c76559be1d24d9f86f47c8818153eb5abb5b7cd
LongPeng <longpeng96@qq.com>
2023-06-18 15:39:17 +0800

    fix bug

--- models/post.go
+++ models/post.go
@@ -6,7 +6,7 @@ import "time"
 // to create a post
 type PostCreated struct {
 	ID          int64     `json:"id" db:"post_id"`
-	AuthorID    int64     `json:"author_id" db:"author_id" binding:"required"`
+	AuthorID    int64     `json:"author_id" db:"author_id"`
 	CommunityID int64     `json:"community_id" db:"community_id binding:"required"`
 	Status      int32     `json:"status" db:"status"`
 	Title       string    `json:"title" db:"title" binding:"required"`
