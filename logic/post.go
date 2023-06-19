0bc3c627e6795a8453a640c5c9cda79c39871bbc
LongPeng <longpeng96@qq.com>
2023-06-19 14:25:33 +0800

    logic.GetPostDetailList finished

--- logic/post.go
+++ logic/post.go
@@ -34,3 +34,36 @@ func GetPostDetailByID(id int64) (*models.ApiPostDetail, error) {
 
 	return apd, err
 }
+
+// GetPostDetailList return a list of length 2
+func GetPostDetailList() (postDetailList []*models.ApiPostDetail, err error) {
+	postList, err := mysql.GetPostDetailList()
+	if err != nil {
+		return nil, err
+	}
+
+	postDetailList = make([]*models.ApiPostDetail, 0, len(postList))
+
+	for _, post := range postList {
+		userData, err := mysql.GetUserByID(post.AuthorID)
+		if err != nil {
+			zap.L().Error("mysql.GetUserByID: ", zap.Error(err))
+			continue
+		}
+
+		communityData, rowIsEmpty, err := mysql.GetCommunityDetail(post.CommunityID)
+		if rowIsEmpty && err == nil {
+			zap.L().Error("mysql.GetCommunityDetail: ", zap.Error(err))
+			continue
+		}
+
+		apd := new(models.ApiPostDetail)
+		apd.CommunityDetailSelected = communityData
+		apd.Post = post
+		apd.AuthorName = userData.Username
+
+		postDetailList = append(postDetailList, apd)
+	}
+
+	return
+}
