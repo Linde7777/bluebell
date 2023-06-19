64d18005e38e4ae774b0b5cab4ae58e12c1fb9ca
LongPeng <longpeng96@qq.com>
2023-06-19 14:24:35 +0800

    rename

--- logic/post.go
+++ logic/post.go
@@ -12,24 +12,24 @@ func CreatePost(pc *models.Post) error {
 }
 
 func GetPostDetailByID(id int64) (*models.ApiPostDetail, error) {
-	postData, err := mysql.GetPostDetailByID(id)
+	post, err := mysql.GetPostDetailByID(id)
 	if err != nil {
 		return nil, err
 	}
 
-	userData, err := mysql.GetUserByID(postData.AuthorID)
+	userData, err := mysql.GetUserByID(post.AuthorID)
 	if err != nil {
 		return nil, err
 	}
 
-	communityData, rowIsEmpty, err := mysql.GetCommunityDetail(postData.CommunityID)
+	communityData, rowIsEmpty, err := mysql.GetCommunityDetail(post.CommunityID)
 	if rowIsEmpty && err == nil {
 		return nil, err
 	}
 
 	apd := new(models.ApiPostDetail)
 	apd.CommunityDetailSelected = communityData
-	apd.Post = postData
+	apd.Post = post
 	apd.AuthorName = userData.Username
 
 	return apd, err
