7a7d39e289cfd5ef11346b31ba1148c39ed878f8
LongPeng <longpeng96@qq.com>
2023-06-20 15:20:22 +0800

    update model

--- models/user.go
+++ models/user.go
@@ -3,7 +3,9 @@ package models
 // UserInserted store the info that is needed
 // to be inserted into database
 type UserInserted struct {
-	UserID   int64  `db:"user_id"`
-	Username string `db:"username"`
-	Password string `db:"password"`
+	UserID       int64  `db:"user_id"`
+	Username     string `db:"username"`
+	Password     string `db:"password"`
+	AccessToken  string
+	RefreshToken string
 }
