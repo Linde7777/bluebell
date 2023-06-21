745e7d5a75b5af3cce0d953246b430186a3a3df5
LongPeng <longpeng96@qq.com>
2023-06-21 16:42:08 +0800

    add parameter structure for vote

--- models/params.go
+++ models/params.go
@@ -12,3 +12,13 @@ type ParamsLogin struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
+
+// ParamsVoteData store the info that is needed to
+// be used in voting a post
+type ParamsVoteData struct {
+	// get UserID from the gin.Context
+	PostID int64 `json:"post_id,string" binding:"required"`
+
+	// 1 for upvote, -1 for downvote, 0 for not voting
+	Direction int8 `json:"direction,string" binding:"required,oneof=1 0 -1"`
+}
