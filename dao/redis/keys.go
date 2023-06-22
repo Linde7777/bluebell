d4a0f1b4e08e299e144b2a61671d948929fdff06
LongPeng <longpeng96@qq.com>
2023-06-22 17:00:06 +0800

    update

--- dao/redis/keys.go
+++ dao/redis/keys.go
@@ -1,8 +1,12 @@
 package redis
 
 const (
-	KeyPrefix              = "bluebell:"
+	Prefix                 = "bluebell:"
 	KeyPostTimeZSet        = "post:time"
 	KeyPostScoreZSet       = "post:score"
 	KeyPostVotedZSetPrefix = "post:voted"
 )
+
+func getRedisKey(key string) string {
+	return Prefix + key
+}
