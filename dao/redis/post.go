ac83805592147b860bc2dbcfbed83b91e762f36b
LongPeng <longpeng96@qq.com>
2023-06-26 20:15:36 +0800

    GetCommunityPostIDsInOrder finished

--- dao/redis/post.go
+++ dao/redis/post.go
@@ -1,14 +1,42 @@
 package redis
 
-import "bluebell/models"
+import (
+	"bluebell/models"
+	"github.com/go-redis/redis"
+	"strconv"
+	"time"
+)
+
+func GetIDsFromKey(key string, page, size int64) ([]string, error) {
+	start := (page - 1) * size
+	end := start + size - 1
+	return rdb.ZRevRange(key, start, end).Result()
+}
 
 func GetPostIDsInOrder(p *models.ParamsPostList) ([]string, error) {
 	key := getRedisKey(KeyPostTimeZSet)
 	if p.Order == models.OrderScore {
 		key = getRedisKey(KeyPostScoreZSet)
 	}
+	return GetIDsFromKey(key, p.Page, p.Size)
+}
 
-	start := (p.Page - 1) * p.Size
-	end := start + p.Size - 1
-	return rdb.ZRevRange(key, start, end).Result()
+func GetCommunityPostIDsInOrder(
+	p *models.ParamsCommunityPostList) ([]string, error) {
+
+	communityIDStr := strconv.Itoa(int(p.CommunityID))
+	communityKey := getRedisKey(KeyCommunityPostSetPrefix + communityIDStr)
+	cacheKey := p.Order + communityIDStr
+	if rdb.Exists(cacheKey).Val() < 1 {
+		pipeline := redis.Pipeline{}
+		pipeline.ZInterStore(cacheKey, redis.ZStore{
+			Aggregate: "MAX",
+		}, communityKey, p.Order)
+		pipeline.Expire(cacheKey, 60*time.Second)
+		if _, err := pipeline.Exec(); err != nil {
+			return nil, err
+		}
+	}
+
+	return GetIDsFromKey(cacheKey, p.Page, p.Size)
 }
