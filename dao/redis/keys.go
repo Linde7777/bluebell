dfe8b35a049290010eea8ae9fa4db61d3b4f9f62
LongPeng <longpeng96@qq.com>
2023-06-26 19:21:44 +0800

    add comment

--- dao/redis/keys.go
+++ dao/redis/keys.go
@@ -1,9 +1,21 @@
 package redis
 
 const (
-	Prefix                 = "bluebell:"
-	KeyPostTimeZSet        = "post:time"
-	KeyPostScoreZSet       = "post:score"
+	Prefix = "bluebell:"
+
+	// KeyPostTimeZSet represent a zset named post:time,
+	// where post_id is the key, the timestamp of when
+	// the post created is the value.
+	KeyPostTimeZSet = "post:time"
+
+	// KeyPostScoreZSet represent a zset named post:score,
+	// where post_id is the key, score is the value.
+	KeyPostScoreZSet = "post:score"
+
+	// KeyPostVotedZSetPrefix act literally as its naming.
+	// For example: post:voted:123456 represent a zset
+	// stored the mapping user_id <-> voting
+	// under a post which id is 123456
 	KeyPostVotedZSetPrefix = "post:voted:"
 )
 
