72ed9d64ca28fc1a8a983636cf1d5a9d0187ff46
LongPeng <longpeng96@qq.com>
2023-06-26 16:29:04 +0800

    create model ParamsPostList

--- models/params.go
+++ models/params.go
@@ -22,3 +22,12 @@ type ParamsVoteData struct {
 	// 1 for upvote, -1 for downvote, 0 for not voting
 	Direction int8 `json:"direction,string" binding:"required,oneof=1 0 -1"`
 }
+
+// ParamsPostList store the data that is
+// passed by query string, which will be used
+// in controller.GetPostDetailList2
+type ParamsPostList struct {
+	Page  int64  `json:"page" form:"page"`
+	Size  int64  `json:"size" form:"size"`
+	Order string `json:"order" form:"order"`
+}
