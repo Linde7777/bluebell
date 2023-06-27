bb13b32fe6a9e9cd30c192c638a0d662ed845b81
LongPeng <longpeng96@qq.com>
2023-06-27 17:02:01 +0800

    added

---
+++ middlewares/ratelimit.go
@@ -0,0 +1,20 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/juju/ratelimit"
+	"net/http"
+	"time"
+)
+
+func RateLimitMiddleware(fillInterval time.Duration, cap int64) func(c *gin.Context) {
+	bucket := ratelimit.NewBucket(fillInterval, cap)
+	return func(c *gin.Context) {
+		if bucket.TakeAvailable(1) < 1 {
+			c.String(http.StatusOK, "rate limit...")
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
