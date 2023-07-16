0e0b7f0c6a313a61097da87bc49f3159cfe276cf
LongPeng <longpeng96@qq.com>
2023-07-16 23:52:43 +0800

    creating tables, v1

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -3,6 +3,7 @@ package mysql
 import (
 	"bluebell/settings"
 	"fmt"
+	"os"
 
 	"go.uber.org/zap"
 
@@ -31,6 +32,62 @@ func Init(cfg *settings.MySQLConfig) (err error) {
 	db.SetMaxOpenConns(viper.GetInt("mysql.max_conns"))
 	db.SetMaxIdleConns(viper.GetInt("mysql.idle_conns"))
 	return
+}
+
+const sqlFilesDir = "../../models"
+const createDBFilename = "create_db.sql"
+
+func CreateDBIfNotExist() error {
+	bytes, err := os.ReadFile(sqlFilesDir + createDBFilename)
+	if err != nil {
+		zap.L().Error("os.ReadFile failed: ", zap.Error(err))
+		return err
+	}
+	script := string(bytes)
+	_, err = db.Exec(script)
+	if err != nil {
+		zap.L().Error("db.Exec failed: ", zap.Error(err))
+		return err
+	}
+
+	return err
+}
+
+const createCommunityTableFilename = "create_community_table.sql"
+const createPostTableFilename = "create_post_table.sql"
+const createUserTableFilename = "create_user_tabel.sql"
+
+func CreateTablesIfNotExist() (err error) {
+	err = createTableIfNotExist(sqlFilesDir + createCommunityTableFilename)
+	if err != nil {
+		return
+	}
+	err = createTableIfNotExist(sqlFilesDir + createPostTableFilename)
+	if err != nil {
+		return err
+	}
+	err = createTableIfNotExist(sqlFilesDir + createUserTableFilename)
+	return err
+}
+
+func InsertCommunityIfNotExist() {
+
+}
+
+func createTableIfNotExist(filepath string) error {
+	bytes, err := os.ReadFile(filepath)
+	if err != nil {
+		zap.L().Error("os.ReadFile failed: ", zap.Error(err))
+		return err
+	}
+	script := string(bytes)
+	_, err = db.Exec(script)
+	if err != nil {
+		zap.L().Error("db.Exec failed: ", zap.Error(err))
+		return err
+	}
+
+	return err
 }
 
 func Close() {
